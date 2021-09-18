dd586b0e086f79ad42704c85a69124d8286cb7f7
alexsh2014 <alexandre.altariba@gmail.com>
2021-09-18 21:45:26 +0200

    ajout penup

--- turtle_triangle.py
+++ turtle_triangle.py
@@ -83,6 +83,7 @@ def fractal(nbCotes, x, y, nbOccurences, rayon):
             goto(p1[0] + x, p1[1] + y)
             pendown()
             goto(p2[0] + x, p2[1] + y)
+            penup()
 
 
 fractal(TRIANGLE, 0, 0, 8, 600)
